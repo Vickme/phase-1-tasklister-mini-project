64b93cb2ae841fa7c9dddaf1fa79ad3b803e3199
victor <victor.njutha@student.moringaschool.com>
2023-10-20 13:51:29 -0700

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  function submitTask() {
+    const taskInput = document.getElementById('create-task-form');
+    const taskText = create-task-form.value;
+  
+    createTaskForm.value = '';
+  
+    const taskList = document.getElementById('list');
+  
+    const taskParagraph = document.createElement('p');
+    taskParagraph.textContent = taskText;
+  
+    taskList.appendChild(taskParagraph);
+  }
+  const submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click', submitTask);
+
 });
